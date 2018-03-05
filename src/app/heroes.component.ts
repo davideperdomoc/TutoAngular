6541c79070ae229e5e5fcee72e522dc6f08474b6
DAVID\daeperdomocr <daeperdomocr@unal.edu.co>
2018-03-05 10:32:01 -0500

    Tuto angular finished

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -1,25 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { Hero } from './hero';
-import { HEROES } from './mock-heroes';
- 
+import { HeroService } from './hero.service';
+
 @Component({
-  selector: 'app-heroes',
+  selector: 'my-heroes',
   templateUrl: './heroes.component.html',
-  styleUrls: ['./heroes.component.css']
+  styleUrls: [ './heroes.component.css' ]
 })
 export class HeroesComponent implements OnInit {
- 
-  heroes = HEROES;
- 
+  heroes: Hero[];
   selectedHero: Hero;
- 
- 
-  constructor() { }
- 
-  ngOnInit() {
+
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
   }
- 
+
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
+
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
+  }
 }
+
+
+/*
+Copyright 2017 Google Inc. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at http://v2.angular.io/license
+*/
